3b8fe16713f0837c88c11ba7f6426625d1c678ac
mgckaled <marcelgckaled@gmail.com>
2023-09-04 16:51:41 -0300

    feat: config auth and init HomePage

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,40 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+import Bottonbar from "@/components/shared/Bottonbar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+
+import Topbar from "@/components/shared/Topbar"
+import "../globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <head></head>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottonbar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
+
+
