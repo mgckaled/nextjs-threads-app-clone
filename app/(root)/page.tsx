8a005cd2c5285a1fc1eaf1fa62e0341ab5c7c6da
mgckaled <marcelgckaled@gmail.com>
2023-09-07 10:09:48 -0300

    feat: create ThreadCard structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={""} // currentUserId={user?.id || ""}
+                currentUserId={""} // currentUserId={user?.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
